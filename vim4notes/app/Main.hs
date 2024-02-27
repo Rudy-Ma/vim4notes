8f749dc9ca0e192c4f9faf93d95f961e92ec4ce8
Rudy-Ma <legorudy@gmail.com>
2024-02-26 21:28:20 -0800

    Create Main.hs

---
+++ vim4notes/app/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+
+import Brick
+import Graphics.Vty
+import Graphics.Vty.Platform.Windows (mkVty)
+
+
+
+ui :: Widget ()
+ui = str "Hello, world!"
+
+main :: IO ()
+main = simpleMain ui
